5315ea7b4392ab205a24109acd652e0ac2cee3f9
stavrianosy <yiannisezn@gmail.com>
2013-03-10 15:57:09 -0700

    BL

---
+++ BMA.BusinessLogic/BaseList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMA.BusinessLogic
+{
+    public abstract class BaseList:ObservableCollection<BaseItem>
+    {
+        public BaseList GetChanges<T>() 
+            where T: BaseList, new()
+        {
+            T obj = new T();
+            var query = this.Where(t => t.HasChanges).ToList();
+            foreach (var item in query)
+                obj.Add(item);
+
+            return obj;
+        }
+    }
+}
