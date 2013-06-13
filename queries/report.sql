6f92d8bce5cb15677b855ee98ed2b0b7cb972654
stavrianosy <yiannisezn@gmail.com>
2013-06-14 02:57:33 +0300

    a

--- queries/report.sql
+++ queries/report.sql
@@ -1,18 +1,34 @@
+DECLARE @installemnt decimal(18,2)
+DECLARE @exp decimal(18,2)
+DECLARE @bills decimal(18,2)
+DECLARE @inc decimal(18,2)
 
 
-select 'installment', sum(amount+tipamount) from [Transaction]
+select @installemnt = sum(amount+tipamount) from [Transaction]
 where 
 TransactionType_TypeTransactionId = 2 and Category_CategoryId = 15 and 
 TransactionDate > '05/25/2013'
 
+select @bills = sum(amount+tipamount) from [Transaction]
+where 
+TransactionType_TypeTransactionId = 2 and Category_CategoryId = 17 and 
+TransactionDate > '05/25/2013'
 
-select 'exp', sum(amount+tipamount) from [Transaction]
+select @exp = sum(amount+tipamount) from [Transaction]
 where 
 TransactionType_TypeTransactionId = 2 and 
 TransactionDate > '05/25/2013'
 
-
-select 'inc', sum(amount+tipamount) from [Transaction]
+select @inc = sum(amount+tipamount) from [Transaction]
 where 
 TransactionType_TypeTransactionId = 1 and 
 TransactionDate > '05/20/2013'
+
+
+select 'installments', @installemnt
+select 'bills', @bills
+select 'expences', @exp
+select 'income', @inc
+select 'balance', @inc - @exp
+select 'days left', datediff(day,GETDATE(), '06/30/2013')
+
